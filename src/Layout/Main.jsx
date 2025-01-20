97fcb95d50c3523c3f0f08e1ae243075c216e593
Adnan <azizulislamadnan@gmail.com>
2025-01-20 23:12:50 +0600

    add: primary installation

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,18 @@
+
+import { Outlet } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
+const Main = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <div className='min-h-screen'>
+            <Outlet></Outlet>
+            </div>
+           <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
