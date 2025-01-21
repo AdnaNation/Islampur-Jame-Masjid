6ad1ce2dfd8b68d9353050ba9c044f6aa218577d
Adnan <azizulislamadnan@gmail.com>
2025-01-21 17:29:49 +0600

    add: menu section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
 import { FiMenu, FiX } from "react-icons/fi";
+import { NavLink } from "react-router-dom";
 
 
 const Navbar = () => {
-
   const [isDashboardOpen, setIsDashboardOpen] = useState(false);
   const toggleDashboard = () => {
     setIsDashboardOpen(!isDashboardOpen);
   };
     return (
-      <div className="text-center flex  items-center bg-gray-600 h-16 fixed w-full">
+      <div className="text-center flex  items-center bg-[#004E5B] h-16 fixed w-full">
 
 <div className="flex  items-center ml-4">
               <button
@@ -24,6 +24,35 @@ const Navbar = () => {
               </button>
             </div>
       <a className="btn btn-ghost text-xl text-white font-bold font-sans flex-1">Islampur Jame Masjid</a>
+
+      {/* dashboard */}
+      <div
+          className={`shadow-lg z-50 bg-[#004E5B] text-white max-w-[180px] ease-in-out transform fixed top-16 left-0  w-[250px]   transition-transform duration-300  ${
+            isDashboardOpen ? "translate-x-0" : "-translate-x-full"
+          }`}
+        >
+      
+
+          <ul className="p-4 space-y-4  max-w-[300px] z-auto  ">
+            <li className="flex justify-between items-center hover:bg-white hover:text-black rounded-md">
+              <NavLink className="border border-1 w-full px-3 py-1 rounded-md" to="/">
+                Home
+              </NavLink>
+            </li>
+            <li className="flex justify-between items-center hover:bg-white hover:text-black rounded-md">
+              <NavLink className="border border-1 w-full px-3 py-1 rounded-md" to="/">
+                Monthly Fee
+              </NavLink>
+            </li>
+            <li className="flex justify-between items-center hover:bg-white hover:text-black rounded-md">
+              <NavLink className="border border-1 w-full px-3 py-1 rounded-md" to="/">
+              চাঁদা
+              </NavLink>
+            </li>
+            
+          </ul>
+        </div>
+
     </div>
     );
 };
