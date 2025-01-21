4aa4c74c9beadcb195ff52171587b11073483fbd
Adnan <azizulislamadnan@gmail.com>
2025-01-21 13:53:36 +0600

    add: menu icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,59 +1,30 @@
+import { useState } from "react";
+import { FiMenu, FiX } from "react-icons/fi";
 
 
 const Navbar = () => {
+
+  const [isDashboardOpen, setIsDashboardOpen] = useState(false);
+  const toggleDashboard = () => {
+    setIsDashboardOpen(!isDashboardOpen);
+  };
     return (
-        <div className="navbar bg-base-100">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor">
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16" />
-              </svg>
+      <div className="text-center flex  items-center bg-gray-600 h-16 fixed w-full">
+
+<div className="flex  items-center ml-4">
+              <button
+                onClick={toggleDashboard}
+                className="text-black hover:text-gray-500 focus:outline-none"
+              >
+                {isDashboardOpen ? (
+                  <FiX className="w-6 h-6 text-white" />
+                ) : (
+                  <FiMenu className="w-6 h-6 text-white" />
+                )}
+              </button>
             </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-              <li><a>Item 1</a></li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li><a>Submenu 1</a></li>
-                  <li><a>Submenu 2</a></li>
-                </ul>
-              </li>
-              <li><a>Item 3</a></li>
-            </ul>
-          </div>
-          <a className="btn btn-ghost text-xl">Islampur Jame Masjid</a>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li><a>Item 1</a></li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                  <li><a>Submenu 1</a></li>
-                  <li><a>Submenu 2</a></li>
-                </ul>
-              </details>
-            </li>
-            <li><a>Item 3</a></li>
-          </ul>
-        </div>
-        <div className="navbar-end">
-          <a className="btn">Button</a>
-        </div>
-      </div>
+      <a className="btn btn-ghost text-xl text-white font-bold font-sans flex-1">Islampur Jame Masjid</a>
+    </div>
     );
 };
 
