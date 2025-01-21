ca254a13eefebd71bd9d6573998ef2c705d593f7
Adnan <azizulislamadnan@gmail.com>
2025-01-22 00:24:26 +0600

    update: menubar setting

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,59 +2,66 @@ import { useState } from "react";
 import { FiMenu, FiX } from "react-icons/fi";
 import { NavLink } from "react-router-dom";
 
-
 const Navbar = () => {
   const [isDashboardOpen, setIsDashboardOpen] = useState(false);
   const toggleDashboard = () => {
     setIsDashboardOpen(!isDashboardOpen);
   };
-    return (
-      <div className="text-center flex  items-center bg-blue-100 top-1 h-14 fixed z-10 w-full rounded-full">
-
-<div className="flex  items-center ml-4">
-              <button
-                onClick={toggleDashboard}
-                className="text-black hover:text-gray-500 focus:outline-none"
-              >
-                {isDashboardOpen ? (
-                  <FiX className="w-6 h-6 text-black" />
-                ) : (
-                  <FiMenu className="w-6 h-6 text-black" />
-                )}
-              </button>
-            </div>
-      <a className="btn btn-ghost text-2xl text-black font-bold font-sans flex-1">Islampur Jame Masjid</a>
+  return (
+    <div className="text-center flex  items-center bg-blue-100 top-1 h-14 fixed z-10 w-full rounded-full">
+      <div className="flex  items-center ml-4">
+        <button
+          onClick={toggleDashboard}
+          className="text-black hover:text-gray-500 focus:outline-none"
+        >
+          {isDashboardOpen ? (
+            <FiX className="w-6 h-6 text-black" />
+          ) : (
+            <FiMenu className="w-6 h-6 text-black" />
+          )}
+        </button>
+      </div>
+      <a className="btn btn-ghost text-2xl text-black font-bold font-sans flex-1">
+        Islampur Jame Masjid
+      </a>
 
       {/* dashboard */}
       <div
-          className={`shadow-lg z-50 bg-blue-100 text-black max-w-[180px] ease-in-out rounded-3xl transform fixed top-16 left-0  w-[250px]   transition-transform duration-300  ${
-            isDashboardOpen ? "translate-x-0" : "-translate-x-full"
-          }`}
-        >
-      
-
-          <ul className="p-4 space-y-4  max-w-[300px] z-auto  ">
-            <li className="flex justify-between items-center hover:bg-white hover:text-black rounded-md">
-              <NavLink className="border border-black border-1 font-bold w-full px-3 py-1 rounded-md" to="/">
-                Home
-              </NavLink>
-            </li>
-            <li className="flex justify-between items-center hover:bg-white hover:text-black rounded-md">
-              <NavLink className="border border-black  border-1 font-bold w-full px-3 py-1 rounded-md" to="/">
-                Monthly Fee
-              </NavLink>
-            </li>
-            <li className="flex justify-between items-center hover:bg-white hover:text-black rounded-md">
-              <NavLink className="border border-black  border-1 font-bold w-full px-3 py-1 rounded-md" to="/">
+        className={`shadow-lg z-50 bg-blue-100 text-black max-w-[180px] ease-in-out rounded-3xl transform fixed top-16 left-0  w-[250px]   transition-transform duration-300  ${
+          isDashboardOpen ? "translate-x-0" : "-translate-x-full"
+        }`}
+      >
+        <ul className="p-4 space-y-4  max-w-[300px] z-auto  ">
+          <li className="flex justify-between items-center hover:bg-white hover:text-black rounded-md">
+            <NavLink
+              onClick={toggleDashboard}
+              className="border border-black border-1 font-bold w-full px-3 py-1 rounded-md"
+              to="/"
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className="flex justify-between items-center hover:bg-white hover:text-black rounded-md">
+            <NavLink
+              onClick={toggleDashboard}
+              className="border border-black  border-1 font-bold w-full px-3 py-1 rounded-md"
+              to="/"
+            >
+              Monthly Fee
+            </NavLink>
+          </li>
+          <li className="flex justify-between items-center hover:bg-white hover:text-black rounded-md">
+            <NavLink onClick={toggleDashboard}
+              className="border border-black  border-1 font-bold w-full px-3 py-1 rounded-md"
+              to="/"
+            >
               চাঁদা
-              </NavLink>
-            </li>
-            
-          </ul>
-        </div>
-
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     </div>
-    );
+  );
 };
 
 export default Navbar;
