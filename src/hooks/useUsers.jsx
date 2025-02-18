6ba865cff44730a1ae44502dd206e76eb2c8993e
Adnan <azizulislamadnan@gmail.com>
2025-02-18 21:51:37 +0600

    add: edit fee

--- src/hooks/useUsers.jsx
+++ src/hooks/useUsers.jsx
@@ -4,14 +4,14 @@ import useAxiosPublic from "./useAxiosPublic";
 
 const useUsers = () => {
     const axiosPublic = useAxiosPublic();
-    const {data: users = [], isPending: isUsersLoading} = useQuery({
+    const {data: users = [], isPending: isUsersLoading, refetch} = useQuery({
          queryKey: ['users'],
          queryFn: async ()=>{
             const res = await axiosPublic.get('/users');
             return res.data
          }
     })
-    return [users, isUsersLoading]
+    return [users, isUsersLoading, refetch]
 };
 
 export default useUsers;
