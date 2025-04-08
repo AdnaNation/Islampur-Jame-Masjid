3f4b827d89d570407c4bb6528e7e69e4a4db8775
Adnan <azizulislamadnan@gmail.com>
2025-04-08 14:29:12 +0600

    add: payment history searching

--- src/pages/PaymentHistory.jsx
+++ src/pages/PaymentHistory.jsx
@@ -1,12 +1,20 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosPublic from "../hooks/useAxiosPublic";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import useHomeName from "../hooks/useHomeName";
 
 const PaymentHistory = () => {
+  const [homeName] = useHomeName();
+  const [home, setHome] = useState('home')
+  const [name, setName] = useState(' ')
   const axiosPublic = useAxiosPublic();
   const { data, refetch } = useQuery({
-    queryKey: ["paymentHistory"],
-    queryFn: async () => await axiosPublic.get("/paymentHistory"),
+    queryKey: ["paymentHistory", home, name],
+    queryFn: async () => await axiosPublic.get(`/paymentHistory?home=${home}&name=${name}`),
+  });
+  const { data: userName= {}, refetch: reload } = useQuery({
+    queryKey: ["usersName", home],
+    queryFn: async () => await axiosPublic.get(`/usersName/${home}`),
   });
   useEffect(() => {
     const interval = setInterval(() => {
@@ -16,8 +24,42 @@ const PaymentHistory = () => {
     return () => clearInterval(interval);
   }, [refetch]);
 
+  const handleHome = async (e)=>{
+    setHome(e.target.value)
+   await reload()
+  }
+  const handleName = (e)=>{
+    setName(e.target.value)
+  }
+
   return (
-    <div className="flex justify-center">
+   <div className="bg-orange-50">
+       <div className="flex justify-center gap-1 my-1">
+       <select onChange={handleHome} className="p-2 border rounded w-40">
+              <option value="home" className="font-bold bg-red-50">
+                বাড়ির নাম
+              </option>
+              {homeName.map((home) => (
+                <option value={home} key={home}>
+                  {" "}
+                  {home}
+                </option>
+              ))}
+            </select>
+            
+       <select disabled={home=== 'home'} onChange={handleName} className="p-2 border rounded w-40">
+              <option value="" className="font-bold bg-red-50">
+                 নাম
+              </option>
+              {userName?.data?.map((name) => (
+                <option value={name.NameBn} key={name._id}>
+                  {" "}
+                  {name.NameBn}
+                </option>
+              ))}
+            </select>
+       </div>
+<div className="flex justify-center">
       <div className="grid md:grid-cols-3 items-center max-w-5xl gap-2 justify-center bg-orange-50">
         {data?.data
           ?.slice()
@@ -40,6 +82,7 @@ const PaymentHistory = () => {
           ))}
       </div>
     </div>
+   </div>
   );
 };
 
