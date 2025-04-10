40dc194b56c8548074d14037aca14c33b68de996
Adnan <azizulislamadnan@gmail.com>
2025-04-10 23:13:43 +0600

    modified: dashboard

--- src/pages/FeePage.jsx
+++ src/pages/FeePage.jsx
@@ -26,6 +26,7 @@ const FeePage = () => {
   const [isOpen4, setIsOpen4] = useState(false);
   const [isOpen5, setIsOpen5] = useState(false);
   const [isOpen6, setIsOpen6] = useState(false);
+  const [seeMore, setSeeMore] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedMonth, setSelectedMonth] = useState("");
   const [id, setId] = useState("");
@@ -97,10 +98,15 @@ const FeePage = () => {
     }
   };
 
+  const handleSeeMore = () => {
+    setSeeMore(true);
+  };
+
   useEffect(() => {
     setFeeRate(userData.FeeRate);
     setDueFee(userData.Due);
     setTarabiFee(userData?.Tarabi?.fee);
+    setSeeMore(false);
     refetch();
     refresh();
   }, [userData, refetch, refresh]);
@@ -342,29 +348,70 @@ const FeePage = () => {
                 <th className="text-center">চাঁদার হার</th>
               </tr>
             </thead>
-            <tbody>
-              {/* row 1 */}
-              {users?.data.map((user, index) => (
-                <tr
-                  className="btn-ghost"
-                  onClick={() => handleUserDetails(user)}
-                  key={user._id}
-                >
-                  {" "}
-                  <th>{index + 1}</th>
-                  <td className="text-[12px]">
-                    <p className="text-center">
-                      {user.NameBn} <br /> {isAdmin && user.Number}
-                    </p>
-                  </td>
-                  <th className="text-[12px] text-center">{user.HomeName}</th>
-                  <td>
-                    <p className="text-center">{user.FeeRate}</p>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
+            {seeMore ? (
+              <tbody>
+                {users?.data.map((user, index) => (
+                  <tr
+                    className="btn-ghost"
+                    onClick={() => handleUserDetails(user)}
+                    key={user._id}
+                  >
+                    {" "}
+                    <th>{index + 1}</th>
+                    <td className="text-[12px]">
+                      <p className="text-center">
+                        {user.NameBn} <br /> {isAdmin && user.Number}
+                      </p>
+                    </td>
+                    <th className="text-[12px] text-center">{user.HomeName}</th>
+                    <td>
+                      <p className="text-center">{user.FeeRate}</p>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            ) : (
+              <tbody>
+                {users?.data.slice(0, 10).map((user, index) => (
+                  <tr
+                    className="btn-ghost"
+                    onClick={() => handleUserDetails(user)}
+                    key={user._id}
+                  >
+                    {" "}
+                    <th>{index + 1}</th>
+                    <td className="text-[12px]">
+                      <p className="text-center">
+                        {user.NameBn} <br /> {isAdmin && user.Number}
+                      </p>
+                    </td>
+                    <th className="text-[12px] text-center">{user.HomeName}</th>
+                    <td>
+                      <p className="text-center">{user.FeeRate}</p>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            )}
           </table>
+          {users?.data?.length > 10 && !seeMore && (
+            <button
+              onClick={handleSeeMore}
+              className="relative z-10 flex items-center justify-center gap-2 px-4 py-1 mx-auto overflow-hidden border-2 rounded-full shadow-xl text-md bg-gray-50 backdrop-blur-md lg:font-semibold isolation-auto border-gray-50 before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-left-full before:hover:left-0 before:rounded-full before:bg-emerald-500 hover:text-gray-50 before:-z-10 before:aspect-square before:hover:scale-150 before:hover:duration-700 group"
+            >
+              আরো দেখুন
+              <svg
+                className="justify-end w-8 h-8 p-2 duration-300 ease-linear rotate-45 border border-gray-700 rounded-full group-hover:rotate-90 group-hover:bg-gray-50 text-gray-50 group-hover:border-none"
+                viewBox="0 0 16 19"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M7 18C7 18.5523 7.44772 19 8 19C8.55228 19 9 18.5523 9 18H7ZM8.70711 0.292893C8.31658 -0.0976311 7.68342 -0.0976311 7.29289 0.292893L0.928932 6.65685C0.538408 7.04738 0.538408 7.68054 0.928932 8.07107C1.31946 8.46159 1.95262 8.46159 2.34315 8.07107L8 2.41421L13.6569 8.07107C14.0474 8.46159 14.6805 8.46159 15.0711 8.07107C15.4616 7.68054 15.4616 7.04738 15.0711 6.65685L8.70711 0.292893ZM9 18L9 1H7L7 18H9Z"
+                  className="fill-gray-800 group-hover:fill-gray-800"
+                />
+              </svg>
+            </button>
+          )}
 
           <dialog id="my_modal_1" className="modal">
             <div className="modal-box bg-orange-50">
