a0afb095d7c89f2b2d5f5db5aa2eb1bcf4749fbd
Adnan <azizulislamadnan@gmail.com>
2025-05-27 11:40:05 +0600

    add: shopkeeer section

--- src/pages/ShopRent.jsx
+++ src/pages/ShopRent.jsx
@@ -1,10 +1,140 @@
+import Swal from "sweetalert2";
+import useAxiosPublic from "../hooks/useAxiosPublic";
+
 const ShopRent = () => {
+  const axiosPublic = useAxiosPublic();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const Name = form.NameEn.value;
+    const NameBn = form.NameBn.value;
+    const Number = form.Number.value;
+    const Rent = form.Rent.value;
+    const Due = 0;
+
+    const PayMonths = [
+      {
+        monthName: "January",
+        status: "unpaid",
+      },
+      {
+        monthName: "February",
+        status: "unpaid",
+      },
+      {
+        monthName: "March",
+        status: "unpaid",
+      },
+      {
+        monthName: "April",
+        status: "unpaid",
+      },
+      {
+        monthName: "May",
+        status: "unpaid",
+      },
+      {
+        monthName: "June",
+        status: "unpaid",
+      },
+      {
+        monthName: "July",
+        status: "unpaid",
+      },
+      {
+        monthName: "August",
+        status: "unpaid",
+      },
+      {
+        monthName: "September",
+        status: "unpaid",
+      },
+      {
+        monthName: "October",
+        status: "unpaid",
+      },
+      {
+        monthName: "November",
+        status: "unpaid",
+      },
+      {
+        monthName: "December",
+        status: "unpaid",
+      },
+    ];
+
+    const user = {
+      Name,
+      NameBn,
+      Number,
+      Rent,
+      Due,
+      PayMonths,
+    };
+    // send data to the server
+    const addUser = await axiosPublic.post("/addShopKeeper", user);
+    if (addUser.data.insertedId) {
+      Swal.fire({
+        title: "Congrats!",
+        text: `${user.NameBn}কে অ্যাড করা হয়েছে!`,
+        icon: "success",
+        confirmButtonText: "Ok",
+      });
+    } else {
+      Swal.fire({
+        icon: "error",
+        text: `${user.NameBn} আগে থেকে অ্যাড আছেন`,
+        showConfirmButton: false,
+        timer: 800,
+      });
+    }
+  };
   return (
     <div className="min-h-screen">
       <div>
-        <h3 className="mt-3 text-xl font-bold text-center">
+        <h3 className="my-3 text-xl font-bold text-center">
           দোকান ভাড়ার তালিকা
         </h3>
+
+        <form onSubmit={handleSubmit}>
+          <div className="px-1 mx-auto space-y-1 form-control md:w-96 w-72">
+            <input
+              type="text"
+              name="NameEn"
+              placeholder="নাম ইংরেজীতে "
+              required
+              className="w-full input input-bordered"
+            />
+
+            <input
+              type="text"
+              name="NameBn"
+              placeholder="নাম বাংলায়"
+              required
+              className="w-full input input-bordered"
+            />
+            <input
+              type="text"
+              name="Rent"
+              placeholder="ভাড়ার পরিমান ইংরেজীতে"
+              required
+              className="w-full input input-bordered"
+            />
+            <input
+              type="text"
+              name="Number"
+              placeholder="নাম্বার 01**"
+              className="w-full input input-bordered"
+            />
+          </div>
+          <div className="mt-2 text-center ">
+            <input
+              type="submit"
+              value="অ্যাড করুন"
+              className="btn btn-outline btn-info"
+            />
+          </div>
+        </form>
       </div>
     </div>
   );
