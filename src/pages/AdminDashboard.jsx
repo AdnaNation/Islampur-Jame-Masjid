630365bfd6f89c02ff371035a40827b3d6a9acaf
Adnan <azizulislamadnan@gmail.com>
2025-07-25 10:36:08 +0600

    add: sweet alert

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -96,15 +96,16 @@ const AdminDashboard = () => {
                   axiosPublic.post("/sms", { number, message });
                   setLoading(false);
                   fresh();
+                } else {
+                  Swal.fire({
+                    position: "top-end",
+                    title: "বকেয়া ম্যাসেজ দেয়ার জন্য কোনো নাম্বার নেই",
+                    showConfirmButton: false,
+                    timer: 800,
+                  });
                 }
               });
           } else {
-            Swal.fire({
-              position: "top-end",
-              title: "বকেয়া ম্যাসেজ দেয়ার জন্য কোনো নাম্বার নেই",
-              showConfirmButton: false,
-              timer: 800,
-            });
             setLoading(false);
           }
         });
