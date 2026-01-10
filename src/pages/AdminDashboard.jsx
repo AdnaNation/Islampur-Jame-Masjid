1a31d07292fce90529c4db74109a80ae5e34881e
AdnaNation <azizulislamadnan@gmail.com>
2026-01-10 21:13:02 +0600

    add

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -286,9 +286,9 @@ const AdminDashboard = () => {
               }-এর হিসাব বন্ধ করুন`}
         </button>
       </div>
-      <button onClick={YearClosed} className="btn">
+      {/* <button onClick={YearClosed} className="btn">
         send year msg
-      </button>
+      </button> */}
     </div>
   );
 };
